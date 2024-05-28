8b8120e7f7cd22688b5b8a84b316feb0de64fc91
ihwan4rfa <ihwanarifandi@gmail.com>
2024-05-28 07:44:35 +0700

    delete all files

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from "react-router-dom"
-import { routeList } from "./routes/routeList"
-
-function App() {
-  const element = useRoutes(routeList)
-  return element;
-}
-
-export default App
